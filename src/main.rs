6888df0e4e0386e0c5447f0eb6d5bc469dc1b36e
Elias Wernicke <e.w.1111111119@gmail.com>
2025-05-27 15:17:04 +0200

    add commands

--- src/main.rs
+++ src/main.rs
@@ -128,27 +128,69 @@ fn main() {
         },
     };
 
+    let currentdate = chrono::Local::now().date_naive();
+
     match cli.command {
         Commands::Mensa { command } => {
-            println!("Mensa command (command: {:?})", command);
-            // println!("Loaded Mensas: {:?}", local_data.keys());
-            // println!("Years of Berliner Tor: {:?}", local_data.get("Mensa Berliner Tor").expect("Name not found").keys());
+            match &command {
+                MensaCommands::Today | MensaCommands::Tomorrow | MensaCommands::Date { .. } => {
+                    let date_to_use = match &command {
+                        MensaCommands::Today => currentdate,
+                        MensaCommands::Tomorrow => currentdate.succ_opt().expect("Failed to get tomorrow's date"),
+                        MensaCommands::Date { date } => {
+                            match chrono::NaiveDate::parse_from_str(date, "%d.%m.%Y") {
+                                Ok(parsed_date) => parsed_date,
+                                Err(_) => {
+                                    println!("Invalid date format. Please use DD.MM.YYYY.");
+                                    return;
+                                },
+                            }
+                        },
+                        _ => panic!("Unexpected command variant"),
+                    };
 
-            print!("\n");
-            let currentdate = chrono::Local::now().date_naive();
-            let food_today = local_data.get("Mensa Berliner Tor")
-                .and_then(|mensa| mensa.get(&currentdate.format("%Y").to_string()))
-                .and_then(|year| year.get(&currentdate.format("%m").to_string()))
-                .and_then(|month| month.get(&currentdate.format("%d").to_string()))
+                    let food_for_date = local_data.get("Mensa Berliner Tor")
+                .and_then(|mensa| mensa.get(&date_to_use.format("%Y").to_string()))
+                .and_then(|year| year.get(&date_to_use.format("%m").to_string()))
+                .and_then(|month| month.get(&date_to_use.format("%d").to_string()))
                 .expect("Data for today not found")
                 .iter()
                 .collect::<Vec<&Meal>>();
-
-            println!("Mensa Berliner Tor");
-            println!("{}", &currentdate.format("%d.%m.%Y"));
-            for food in food_today {
-                println!();
-                println!("{}", food);
+                    println!("Mensa Berliner Tor");
+                    println!("{}", date_to_use.format("%Y-%m-%d"));
+                    for food in food_for_date {
+                        println!();
+                        println!("{}", food);
+                    }
+                },
+                MensaCommands::Settings { command } => {
+                    match command {
+                        SettingsCommands::Primary { mensa } => {
+                            println!("Setting primary mensa to: {}", mensa);
+                            // Here you would set the primary mensa
+                        },
+                        SettingsCommands::Add { mensa } => {
+                            println!("Adding mensa: {}", mensa);
+                            // Here you would add a mensa
+                        },
+                        SettingsCommands::Remove { mensa } => {
+                            println!("Removing mensa: {}", mensa);
+                            // Here you would remove a mensa
+                        },
+                        SettingsCommands::List => {
+                            println!("Listing all mensas.");
+                            // Here you would list all available mensas
+                        },
+                        SettingsCommands::Occupation { occupation } => {
+                            println!("Setting occupation to: {}", occupation);
+                            // Here you would set the occupation
+                        },
+                        SettingsCommands::Extras { extras } => {
+                            println!("Setting extras to: {}", extras);
+                            // Here you would set the extras
+                        },
+                    }
+                },
             }
         },
         Commands::Events { event } => {
